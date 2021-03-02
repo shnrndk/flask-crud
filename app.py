931ce6d99e4df241526b1cf8cee749ba862907ba
Sahan <shnrndk@gmail.com>
2021-03-02 19:15:59 +0530

    add product

--- app.py
+++ app.py
@@ -43,6 +43,20 @@ class ProductSchema(ma.Schema):
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
+#Create a product
+@app.route('/product',methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+   
+    new_product = Product(name,description,price,qty)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
 
 # Run Server
 if __name__ == '__main__':
