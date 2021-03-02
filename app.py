0826643f7f5071e23ac69c803b434bddb17d1a90
Sahan <shnrndk@gmail.com>
2021-03-02 18:05:42 +0530

    first test get request

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import os
 # init app
 app = Flask(__name__)
 
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg': 'Hello world'})
+
+
 # Run Server
 if __name__ == '__main__':
     app.run(debug=True)
