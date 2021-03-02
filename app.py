656a95605637fe2a4d5f944f3143869c8643d758
Sahan <shnrndk@gmail.com>
2021-03-02 19:15:18 +0530

    db setup

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from enum import unique
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -5,11 +6,42 @@ import os
 
 # init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init Db
+db = SQLAlchemy(app)
+
+# Init mn
+ma = Marshmallow(app)
+
+# Product Class/Modal
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id", "name", "description", "price", "qty")
 
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg': 'Hello world'})
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
 
 # Run Server
